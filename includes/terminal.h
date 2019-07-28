add5be8fbce8973b1b9bf62c1a8e83d532a10d51
Alan Barnett <alanbarnett328@gmail.com>
2019-07-28 08:07:12 -0700

    refactor(terminal): better error handling
    Print debug messages when things fail, and do better checking for failure.
    Return error codes instead of doing error printing, using error.h.

--- includes/terminal.h
+++ includes/terminal.h
@@ -6,14 +6,15 @@
 /*   By: abarnett <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/27 08:17:05 by abarnett          #+#    #+#             */
-/*   Updated: 2019/07/27 08:39:07 by abarnett         ###   ########.fr       */
+/*   Updated: 2019/07/28 03:11:12 by abarnett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TERMINAL_H
 # define TERMINAL_H
 
-#include <termios.h>
+# include "error.h"
+# include <termios.h>
 
 struct	s_my_term
 {
@@ -21,7 +22,7 @@ struct	s_my_term
 	struct termios	new_term;
 };
 
-int		terminal_setup(void);
-void	terminal_restore(void);
+enum e_err_code	terminal_setup(void);
+enum e_err_code	terminal_restore(void);
 
 #endif
--- srcs/terminal.c
+++ srcs/terminal.c
@@ -6,37 +6,51 @@
 /*   By: abarnett <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/27 08:18:10 by abarnett          #+#    #+#             */
-/*   Updated: 2019/07/27 09:26:19 by abarnett         ###   ########.fr       */
+/*   Updated: 2019/07/28 03:12:52 by abarnett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "debug.h"
+#include "error.h"
 #include "terminal.h"
 #include <termios.h>
 #include <unistd.h>
 
 static struct s_my_term	g_term;
 
-int		terminal_setup(void)
+enum e_err_code	terminal_setup(void)
 {
-	int	err;
-
-	err = tcgetattr(0, &g_term.old_term);
+	if (tcgetattr(STDIN_FILENO, &g_term.old_term) != 0)
+	{
+		print_debug("Failed to save old terminal attributes");
+		return (E_TCGETAT_FAIL);
+	}
 	print_debug("Saved old attributes");
-	if (err != 0)
-		return (1);
-	tcgetattr(STDIN_FILENO, &g_term.new_term);
+	if (tcgetattr(STDIN_FILENO, &g_term.new_term) != 0)
+	{
+		print_debug("Failed to save new terminal attributes");
+		return (E_TCGETAT_FAIL);
+	}
 	print_debug("Saved new attributes");
 	g_term.new_term.c_lflag &= ~(ICANON|ECHO);
 	g_term.new_term.c_cc[VMIN] = 1;
 	g_term.new_term.c_cc[VTIME] = 0;
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &g_term.new_term);
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &g_term.new_term) != 0)
+	{
+		print_debug("Failed to enter non-canonical mode");
+		return (E_TCSETAT_FAIL);
+	}
 	print_debug("Enabled non-canonical mode");
 	return (0);
 }
 
-void	terminal_restore(void)
+enum e_err_code	terminal_restore(void)
 {
-	tcsetattr(STDIN_FILENO, TCSANOW, &g_term.old_term);
+	if (tcsetattr(STDIN_FILENO, TCSANOW, &g_term.old_term) != 0)
+	{
+		print_debug("Failed to restore old terminal state");
+		return (E_TCSETAT_FAIL);
+	}
 	print_debug("Reset terminal settings");
+	return (0);
 }
