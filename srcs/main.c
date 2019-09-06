b7378f48b8912b0adea955c9f352fa89c09ea6ae
Alan Barnett <alanbarnett328@gmail.com>
2019-09-05 20:52:50 -0700

    refactor(main): remove terminal setup/restore
    Removes terminal setup and restore from the main, which will be handled
    in the actual ft_select function

--- srcs/main.c
+++ srcs/main.c
@@ -6,35 +6,20 @@
 /*   By: alan <alanbarnett328@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/26 00:10:33 by alan              #+#    #+#             */
-/*   Updated: 2019/07/28 03:11:27 by abarnett         ###   ########.fr       */
+/*   Updated: 2019/09/05 20:51:44 by abarnett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "debug.h"
-#include "error.h"
-#include "terminal.h"
 #include "ft_select.h"
 #include "ft_printf.h"
-#include <termios.h>
-#include <unistd.h>
 
 int		main(int argc, char **argv)
 {
-	enum e_err_code	err;
 	const char		*selected;
 
-	if (!isatty(STDIN_FILENO))
-	{
-		return (print_error(E_NOTATTY));
-	}
-	print_debug("Setting up terminal");
-	if ((err = terminal_setup()) != 0)
-	{
-		select_exit(err);
-	}
-	print_debug("Selecting");
+	if (argc < 2)
+		return (0);
 	selected = ft_select(argc - 1, argv + 1);
 	ft_printf("%s\n", selected);
-	print_debug("Resetting and quitting");
-	select_exit(0);
+	return (0);
 }
