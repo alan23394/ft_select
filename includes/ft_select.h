cc6fa589f033644a3807b72a04a45f8aeda6fc8e
Alan Barnett <alanbarnett328@gmail.com>
2019-10-27 05:10:08 -0700

    add(main struct): init function and definition
    Add struct with normal select information, like the amount of columns
    and the size of my terminal among other things. Also add a function to
    initialize it's members to 0, because since I only really need one of
    these structs, I'm not actually allocating it, I'm just using a struct
    whose address I pass around.

--- includes/ft_select.h
+++ includes/ft_select.h
@@ -6,7 +6,7 @@
 /*   By: abarnett <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/27 08:09:56 by abarnett          #+#    #+#             */
-/*   Updated: 2019/08/29 22:01:14 by abarnett         ###   ########.fr       */
+/*   Updated: 2019/10/27 00:12:32 by alan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,38 @@
 # define FT_SELECT_H
 
 # include "error.h"
+# include "point.h"
+
+# define DEFAULT_PADDING 4
+
+/*
+** combine selected strings into output string
+**
+** select struct
+** iterator list of strings
+** stack of deleted strings
+** string for final output
+** overall widest string
+** word trimming length
+** number of string columns
+** s_point for window size
+**	lines in terminal
+**	columns in terminal
+*/
+
+struct s_select
+{
+	struct s_iter	*strings;
+//	struct s_stack	*deleted;
+	const char		*output;
+	unsigned int	str_maxlen;
+	unsigned int	str_wraplen;
+	unsigned int	str_padding;
+	unsigned int	sel_columns;
+	struct s_point	term_size;
+};
+
+void			select_info_init(struct s_select *select_info);
 
 enum e_err_code	ft_select(const char **selected, int num_args, char **args);
 enum e_err_code	select_return(enum e_err_code error_code);
