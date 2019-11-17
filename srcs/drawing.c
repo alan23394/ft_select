5d57e77278d7e64e3da8ea01511861bef24d210f
Alan Barnett <alanbarnett328@gmail.com>
2019-11-17 06:30:08 -0800

    update(drawing): remove termcaps, add in config
    Removes termcaps things by using my ft_termcaps sublibrary (to move the
    cursor). The other printing can be done with ft_printf, because we don't
    need tputs to print any padding or anything for normal strings. Also use
    the macros in my config file to properly trim the string, and do
    underlining and highlighting in a single printf statement (actually
    there are 4 different printf statements per function, but only one of
    them needs executed based on the case. I would like to simplify this
    further, which I definitely can.)

--- srcs/drawing.c
+++ srcs/drawing.c
@@ -6,28 +6,17 @@
 /*   By: alan <alanbarnett328@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/27 04:47:56 by alan              #+#    #+#             */
-/*   Updated: 2019/10/27 05:05:06 by alan             ###   ########.fr       */
+/*   Updated: 2019/11/10 08:19:56 by alan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "select_config.h"
 #include "select_info.h"
 #include "select_string.h"
+#include "ft_printf.h"
 #include "ft_dnode.h"
 #include "ft_iter.h"
-#include <curses.h>
-#include <term.h>
-#include <unistd.h>
-
-/*
-** Printing function I need to work with subshells
-** STDIN_FILENO still references the terminal when in a subshell
-*/
-/* TODO doesn't need to be in here, split termcaps stuff into it's own thing */
-
-int		print_char_stdin(int c)
-{
-	return (write(STDIN_FILENO, &c, 1));
-}
+#include "ft_termcaps.h"
 
 /*
 ** draw string and draw cursor string, combined with move to gridpos
@@ -38,46 +27,63 @@ int		print_char_stdin(int c)
 ** they both need to draw reverse video if the string is selected
 */
 
-void	move_to_gridpos(struct s_select *info, unsigned int x, unsigned int y)
+static void	move_to_gridpos(struct s_select *info,
+							unsigned int x, unsigned int y)
 {
-	char			*termcaps_str;
-
-	x = (x * info->str_maxlen) + (x * info->str_padding);
-	termcaps_str = tgetstr("cm", 0);
-	termcaps_str = tparm(termcaps_str, y, x);
-	tputs(termcaps_str, 1, print_char_stdin);
+	x = (x * info->sel_column_width) + (x * SELECT_STRING_PADDING);
+	ft_tc_cursor_move(y, x);
 }
+
+/*
+** TODO make these better! print \e[0m at the end and check if you need to
+** print the starting character instead of a bunch of different printfs
+** or make a before and after variable you can set at once
+** print the whole thing once with the stuff considered
+*/
 
 void	draw_string(struct s_select *info, struct s_select_string *string)
 {
 	move_to_gridpos(info, string->pos.x, string->pos.y);
 	if (string->selected)
-	{
-		tputs("\e[7m", 1, print_char_stdin);
-		tputs(string->str, 1, print_char_stdin);
-		tputs("\e[0m", 1, print_char_stdin);
-	}
+		if (string->str_len > info->sel_column_width)
+			ft_printfd(0, "%s%.*s%.*s%s", "\e[7m",
+					info->sel_column_width - SELECT_TRIM_STRLEN, string->str,
+					SELECT_TRIM_STRLEN, SELECT_TRIM_STR,
+					"\e[0m");
+		else
+			ft_printfd(0, "%s%.*s%s",
+					"\e[7m", info->sel_column_width, string->str, "\e[0m");
 	else
-	{
-		tputs(string->str, 1, print_char_stdin);
-	}
+		if (string->str_len > info->sel_column_width)
+			ft_printfd(0, "%.*s%.*s",
+					info->sel_column_width - SELECT_TRIM_STRLEN, string->str,
+					SELECT_TRIM_STRLEN, SELECT_TRIM_STR);
+		else
+			ft_printfd(0, "%.*s", info->sel_column_width, string->str);
 }
 
 void	draw_cursor_string(struct s_select *info, struct s_select_string *string)
 {
 	move_to_gridpos(info, string->pos.x, string->pos.y);
 	if (string->selected)
-	{
-		tputs("\e[4;7m", 1, print_char_stdin);
-		tputs(string->str, 1, print_char_stdin);
-		tputs("\e[0m", 1, print_char_stdin);
-	}
+		if (string->str_len > info->sel_column_width)
+			ft_printfd(0, "%s%.*s%.*s%s", "\e[4;7m",
+					info->sel_column_width - SELECT_TRIM_STRLEN, string->str,
+					SELECT_TRIM_STRLEN, SELECT_TRIM_STR,
+					"\e[0m");
+		else
+			ft_printfd(0, "%s%.*s%s",
+					"\e[4;7m", info->sel_column_width, string->str, "\e[0m");
 	else
-	{
-		tputs("\e[4m", 1, print_char_stdin);
-		tputs(string->str, 1, print_char_stdin);
-		tputs("\e[0m", 1, print_char_stdin);
-	}
+		if (string->str_len > info->sel_column_width)
+			ft_printfd(0, "%s%.*s%.*s%s", "\e[4m",
+					info->sel_column_width - SELECT_TRIM_STRLEN, string->str,
+					SELECT_TRIM_STRLEN, SELECT_TRIM_STR,
+					"\e[0m");
+		else
+			ft_printfd(0, "%s%.*s%s", "\e[4m",
+					info->sel_column_width, string->str,
+					"\e[0m");
 }
 
 void	draw_screen(struct s_select *info)
