93515eba037c0e500207919b8d3a67425f4fedc8
Alan Barnett <alanbarnett328@gmail.com>
2019-11-18 08:47:03 -0800

    remove(select_return): doesn't need to be public
    Removes function that I have made static in the ft_select file, since it
    only gets used from there for now.

--- includes/ft_select.h
+++ includes/ft_select.h
@@ -6,7 +6,7 @@
 /*   By: abarnett <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/27 08:09:56 by abarnett          #+#    #+#             */
-/*   Updated: 2019/11/08 18:47:52 by alan             ###   ########.fr       */
+/*   Updated: 2019/11/18 08:29:43 by alan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,5 @@ int				setup_screen(void);
 int				restore_screen(void);
 
 enum e_err_code	ft_select(const char **selected, int num_args, char **args);
-enum e_err_code	select_return(enum e_err_code error_code);
 
 #endif
