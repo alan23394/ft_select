59919ebeb415aea940abbb85c1d4e1c41f0cf3dd
Alan Barnett <alanbarnett328@gmail.com>
2019-11-18 12:30:12 -0800

    update(select_terminal): move termcaps init
    Remove termcaps init from setting up the terminal. This can be done
    earlier, and it will help this function from having multiple unrelated
    return codes. Now this function's entire job is to create my terminal
    structs, set up the important noncanonical things, and then switch to
    the new terminal. I'm about to change how init leaves as well, I'll make
    it delete the terminal struct if the particular setup function I used
    "failed" (as of now, there isn't really a failure, I'm just allowing for
    a more complicated setup function that may have some failure condition).
    That way, I don't have to worry about whether or not to free memory if
    that function doesn't work. I find it's better to know that a function
    who failed has cleaned itself up.

--- srcs/select_screen.c
+++ srcs/select_screen.c
@@ -6,7 +6,7 @@
 /*   By: alan <alanbarnett328@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/28 14:55:36 by alan              #+#    #+#             */
-/*   Updated: 2019/11/06 21:10:05 by alan             ###   ########.fr       */
+/*   Updated: 2019/11/18 12:24:50 by alan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,36 @@
 
 /*
 ** Returns 0 on success, 1 on failure.
+** As of yet, there isn't really a failure case. If we don't have an alternate
+** screen (like on the tty), we can just clear the screen and it will be fine.
+** If the cursor can't be invisible, then oh well, it doesn't really mean the
+** program needs to quit. Pretty much every terminal will support the clear
+** termcap, and it's not unreasonable to assume that if we don't have the clear
+** termcap, then tgetent would have failed at the beginning (in the case of a
+** missing term variable).
 */
 
 int	setup_screen(void)
 {
 	if (ft_tc_put(TC_ENABLE_ALT_SCREEN) != 0)
-		return (1);
-	if (ft_tc_put(TC_CURSOR_INVISIBLE) != 0)
-		return (1);
+	{
+		ft_tc_put(TC_CLEAR);
+	}
+	ft_tc_put(TC_CURSOR_INVISIBLE);
 	return (0);
 }
+
+/*
+** TODO: Jump to last line instead of clearing if we can't leave the alternate
+** screen.
+*/
 
 int	restore_screen(void)
 {
 	if (ft_tc_put(TC_DISABLE_ALT_SCREEN) != 0)
-		return (1);
-	if (ft_tc_put(TC_CURSOR_VISIBLE) != 0)
-		return (1);
+	{
+		ft_tc_put(TC_CLEAR);
+	}
+	ft_tc_put(TC_CURSOR_VISIBLE);
 	return (0);
 }
--- srcs/select_terminal.c
+++ srcs/select_terminal.c
@@ -6,12 +6,11 @@
 /*   By: abarnett <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/27 08:18:10 by abarnett          #+#    #+#             */
-/*   Updated: 2019/11/16 08:37:39 by alan             ###   ########.fr       */
+/*   Updated: 2019/11/18 06:32:42 by alan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "select_terminal.h"
-#include "select_debug.h"
 #include "error.h"
 #include "ft_term.h"
 #include "ft_termcaps.h"
@@ -33,10 +32,6 @@ static int		select_term_setup(struct termios *term)
 
 enum e_err_code	terminal_setup(void)
 {
-	if (ft_tc_setup() != 0)
-	{
-		return (E_NOTERMINFO);
-	}
 	if (init_terms(select_term_setup) != 0)
 	{
 		return (E_TINIT_FAIL);
@@ -54,7 +49,6 @@ enum e_err_code	terminal_restore(void)
 	{
 		return (E_TRESTORE_FAIL);
 	}
-	PRINT_DEBUG("Restored previous terminal state successfully");
 	delete_terms();
 	return (0);
 }
