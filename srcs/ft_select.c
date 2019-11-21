b497e88096e786805d845070cc681e701256d993
Alan Barnett <alanbarnett328@gmail.com>
2019-11-21 02:36:14 -0800

    feat(select_loop): create strings, and select them
    Use the functions I've created to make the strings for select, and get
    input in a loop and run functions with it.

--- srcs/ft_select.c
+++ srcs/ft_select.c
@@ -12,9 +12,36 @@
 
 #include "ft_select.h"
 #include "select_terminal.h"
+#include "select_info.h"
+#include "select_keys.h"
 #include "select_debug.h"
 #include "error.h"
+#include "drawing.h"
+#include "ft_iter.h"
 #include <unistd.h>
+#include <stdint.h>
+
+const char			*ft_select_loop(struct s_select *info)
+{
+	uint32_t		input;
+	int				ret;
+
+	while (42)
+	{
+		input = 0;
+		ret = read(STDIN_FILENO, &input, 4);
+		if (ret)
+		{
+			if (input >= 128)
+				ret = handle_arrow_key(input, info);
+			else
+				ret = handle_char_input((char)input, info);
+			if (!ret)
+				break;
+		}
+	}
+	return (info->output);
+}
 
 static enum e_err_code	select_return(enum e_err_code error_code)
 {
@@ -52,5 +79,10 @@ enum e_err_code			ft_select(const char **selected, int argc, char **argv)
 	{
 		PRINT_DEBUG("Can't properly set up screen");
 	}
+	select_info_init(&select_info);
+	select_info.strings = make_select_iter(argc, argv, &(select_info.str_maxlen));
+	calibrate_screen(&select_info);
+	draw_screen(&select_info);
+	*selected = ft_select_loop(&select_info);
 	return (select_return(0));
 }
